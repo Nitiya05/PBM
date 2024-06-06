7cf24708bd12e244c9a17142989c2bf9089bddc5
Nitiya05 <nitiyarihaisy@gmail.com>
2024-06-06 10:30:34 +0700

    commit

--- app/src/main/java/com/dicoding/tesya/splash/SplashActivity.kt
+++ app/src/main/java/com/dicoding/tesya/splash/SplashActivity.kt
@@ -16,23 +16,16 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // This is used to hide the status bar and make
-        // the splash screen as a full screen activity.
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        // we used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
-        //Normal Handler is deprecated , so we have to change the code little bit
-
-        // Handler().postDelayed({
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, OnboardingActivity::class.java)
             startActivity(intent)
 
             finish()
-        }, 3000) // 3000 is the delayed time in milliseconds.
+        }, 3000)
     }
 }
